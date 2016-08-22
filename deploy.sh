7f8941f8b267dbf2a15f14961d6f22781bccb31e
Christophe Antoniewski <drosofff@gmail.com>
2016-08-22 12:33:45 +0200

    annotate .travis.yml and deploy.sh files

--- deploy.sh
+++ deploy.sh
@@ -10,7 +10,7 @@ mkdir -p docs/html
     git init
     git config user.name "${GH_USER_NAME}"
     git config user.email "${GH_USER_EMAIL}"
-    git remote add upstream "https://$GH_TOKEN@${GH_REF}"
+    git remote add upstream "https://$GH_TOKEN@${GH_REF}"  # may be changed
     git fetch upstream
     git reset upstream/gh-pages
 )
@@ -29,9 +29,10 @@ mkdocs build --clean --site-dir docs/html
 cd $SCRIPT_PATH
 
 # Next we upload to docker
-docker tag galaxy_kickstart artbio/galaxy-kickstart-base:$TRAVIS_COMMIT
+docker tag galaxy_kickstart artbio/galaxy-kickstart-base:$TRAVIS_COMMIT  # $TRAVIS_COMMIT is a standard env variable
 docker tag galaxy_kickstart artbio/galaxy-kickstart-base:latest
-LOGIN="docker login -e=$DOCKER_EMAIL -u=$DOCKER_USERNAME -p=$DOCKER_PASSWORD"
+LOGIN="docker login -e=$DOCKER_EMAIL -u=$DOCKER_USERNAME -p=$DOCKER_PASSWORD"  # defined in the Travis repository settings
 $LOGIN || (sleep 5s && $LOGIN || echo "login failed twice, quitting" && exit 1)
 docker push artbio/galaxy-kickstart-base:$TRAVIS_COMMIT || (sleep 5s && docker push artbio/galaxy-kickstart-base:$TRAVIS_COMMIT || echo "push failed twice, quitting" && exit 1)
 docker push artbio/galaxy-kickstart-base:latest || (sleep 5s && docker push artbio/galaxy-kickstart-base:latest || echo "push failed twice, quitting" && exit 1)
+# if additional flavors, you would build them and push them there
